0a69a63b891c67612f1e5d109725ebca18962591
Nader Ganayem <nadergan@gmail.com>
2023-02-20 13:33:30 +0200

    Javadoc, remove Yaml, tidy

--- src/main/java/com/k2view/agent/K2ViewAgent.java
+++ src/main/java/com/k2view/agent/K2ViewAgent.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.FileNotFoundException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -16,31 +15,38 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.yaml.snakeyaml.Yaml;
-
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.Map;
 
-
+/**
+ * The main class for the K2ViewAgent program.
+ */
 public class K2ViewAgent {
 
+    /**
+     * The logger used for the K2ViewAgent program.
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(K2ViewAgent.class);
-    private static final Map<String, Object> config = YamlToMap("k2view-agent.yaml");
 
+    /**
+     * The polling interval in seconds for the K2ViewAgent program.
+     */
     private static final int pollingInterval = 60;
+
+    /**
+     * The queue that holds the requests to be processed by the K2ViewAgent program.
+     */
     private static ConcurrentLinkedQueue<Map<String, Object>> requestsQueue = new ConcurrentLinkedQueue<>();
-    private static ConcurrentLinkedQueue<JsonObject> responseQueue = new ConcurrentLinkedQueue<>();
 
+    /**
+     * The main method that runs the K2ViewAgent program.
+     *
+     * @param args The command line arguments for the program.
+     */
     public static void main(String[] args) {
-
-//        ConcurrentLinkedQueue<String> requestsQueue = new ConcurrentLinkedQueue<>();
-
         // Start the first thread that reads a list of URLs
         Thread managerThread = new Thread(() -> {
             while (true) {
-                List<Map<String, Object>> requests = getRequests();
-                ArrayList<String> urls = getUrls(requests);
+                List<Map<String, Object>> requests = getInboxMessages();
 
                 for (Map<String, Object> req : requests) {
                     requestsQueue.add(req);
@@ -61,7 +67,9 @@ public class K2ViewAgent {
         Thread workerThread = new Thread(() -> {
           AgentSender agentSender = new AgentSender(10);
            while (true) {
+                int requestsCounter = 0;
                 while (!requestsQueue.isEmpty()) {
+                    requestsCounter ++;
                     Map<String, Object> req = requestsQueue.poll();
                     String url = req.get("url").toString().trim();
                     ///
@@ -86,14 +94,17 @@ public class K2ViewAgent {
                List<AgentSender.Response> mailResponses = null;
                try {
                    mailResponses = responses.get();
+                   int numberOfResponses = 0;
                    if (! mailResponses.isEmpty()) {
                        Map<String, Object> responseMap = new HashMap<>();
                        for (AgentSender.Response response: mailResponses) {
-                           responseMap.put("id", response.id().toString());
+                           responseMap.put("id", (int)Double.parseDouble(response.id()));
                            responseMap.put("body", response.body().trim());
                            responseMap.put("status", response.code());
                            JsonObject responseJson = new Gson().toJsonTree(responseMap).getAsJsonObject();
                            System.out.println(responseJson);
+                           numberOfResponses++;
+                           if (numberOfResponses == requestsCounter) agentSender.close();
 
                        }
                    }
@@ -103,7 +114,6 @@ public class K2ViewAgent {
                } catch (ExecutionException e) {
                    throw new RuntimeException(e);
                }
-
            }
 
         });
@@ -112,48 +122,32 @@ public class K2ViewAgent {
 
     }
 
-    private static List<Map<String, Object>> getRequests() {
+    /**
+     * Gets a list of requests from the manager's inbox via REST API.
+     *
+     * @return A list of requests as a list of maps, or null if an error occurred.
+     */
+    private static List<Map<String, Object>> getInboxMessages() {
         // Replace this code with the logic to read the URLs from the REST API
-        String url = config.get("MANAGER_URL").toString();
+        String url = System.getenv("K2VIEW_MANAGER_URL");
 
         LOGGER.info("MANAGER URL: " + url);
         HttpClient client = HttpClient.newBuilder().build();
-        ArrayList<String> urls = new ArrayList<>();
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .uri(URI.create(url))
                 .build();
-
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             String jsonArrayString = response.body();
             Gson gson = new Gson();
-            List<Map<String, Object>> listOfMaps = gson.fromJson(jsonArrayString, new TypeToken<List<Map<String, Object>>>() {}.getType());
-            return listOfMaps;
+            List<Map<String, Object>> bodyToJson = gson.fromJson(jsonArrayString, new TypeToken<List<Map<String, Object>>>() {}.getType());
+            return bodyToJson;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
-    
-    private static ArrayList<String> getUrls(List<Map<String, Object>> requests) {
-        // Replace this code with the logic to read the URLs from the REST API
-        ArrayList<String> urls = new ArrayList<>();
 
-        for (Map<String, Object> t: requests) {
-            urls.add(t.get("url").toString());
-        }
-        return urls;
-    }
 
-    private static Map<String, Object> YamlToMap(String path) {
-        Yaml yaml = new Yaml();
-        InputStream inputStream;
-        try {
-            inputStream = new FileInputStream(path);
-            return yaml.load(inputStream);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
